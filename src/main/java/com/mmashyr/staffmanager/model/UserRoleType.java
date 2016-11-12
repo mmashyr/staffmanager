b49a08e033b5a10c95334362cb028ba8d01724ee
Mashyr Mark <mashyr.mark@gmail.com>
2016-11-12 15:14:38 +0200

    Enum of all existing roles created

---
+++ src/main/java/com/mmashyr/staffmanager/model/UserRoleType.java
@@ -0,0 +1,20 @@
+package com.mmashyr.staffmanager.model;
+
+/**
+ * Created by Mark on 11.11.2016.
+ */
+public enum UserRoleType {
+    USER, ADMIN
+//    USER("user"),
+//    ADMIN("admin");
+
+//    String role;
+//
+//        Roles(String role) {
+//        this.role = role;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+}
