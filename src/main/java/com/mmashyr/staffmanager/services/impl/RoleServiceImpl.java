3c7ee6789b8447558d044d48616b12ba044961cd
Mashyr Mark <mashyr.mark@gmail.com>
2016-11-12 15:51:57 +0200

    Transactions added

--- src/main/java/com/mmashyr/staffmanager/services/impl/RoleServiceImpl.java
+++ src/main/java/com/mmashyr/staffmanager/services/impl/RoleServiceImpl.java
@@ -5,6 +5,8 @@ import com.mmashyr.staffmanager.persistence.RoleDao;
 import com.mmashyr.staffmanager.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,32 +20,38 @@ public class RoleServiceImpl implements RoleService {
     RoleDao dao;
 
     @Override
+    @Transactional(readOnly = true, propagation= Propagation.SUPPORTS)
     public List<Role> getAll() {
         return dao.getAll();
     }
 
     @Override
+    @Transactional(readOnly = true, propagation=Propagation.SUPPORTS)
     public Role getById(long id) {
         return dao.getById(id);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void add(Role role) {
         dao.add(role);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void update(Role role) {
         dao.update(role);
 
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void delete(long id) {
         dao.delete(id);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public Role findByType(String type) {
         return dao.findByType(type);
     }
--- src/main/java/com/mmashyr/staffmanager/services/impl/UserServiceImpl.java
+++ src/main/java/com/mmashyr/staffmanager/services/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.mmashyr.staffmanager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,32 +24,38 @@ public class UserServiceImpl implements UserService {
     PasswordEncoder passwordEncoder;
 
     @Override
+    @Transactional(readOnly = true, propagation= Propagation.SUPPORTS)
     public List<User> getAll() {
         return dao.getAll();
     }
 
     @Override
+    @Transactional(readOnly = true, propagation=Propagation.SUPPORTS)
     public User getById(long id) {
         return dao.getById(id);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void add(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         dao.add(user);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void update(User user) {
         dao.update(user);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void delete(long id) {
         dao.delete(id);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public User findByLogin(String login) {
         return dao.findByLogin(login);
     }
