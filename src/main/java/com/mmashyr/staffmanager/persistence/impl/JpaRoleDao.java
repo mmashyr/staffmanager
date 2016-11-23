63c8890d4966442122eee09ea5630ba832a90486
Mashyr Mark <mashyr.mark@gmail.com>
2016-11-23 15:16:02 +0200

    jpaRole and jpaUser DAOs now return null if getSingleResult() has no results

--- src/main/java/com/mmashyr/staffmanager/persistence/impl/JpaRoleDao.java
+++ src/main/java/com/mmashyr/staffmanager/persistence/impl/JpaRoleDao.java
@@ -1,24 +1,27 @@
 package com.mmashyr.staffmanager.persistence.impl;
 
 import com.mmashyr.staffmanager.model.Role;
-import com.mmashyr.staffmanager.model.User;
 import com.mmashyr.staffmanager.model.UserRoleType;
 import com.mmashyr.staffmanager.persistence.RoleDao;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Query;
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
 /**
  * Created by Mark on 12.11.2016.
  */
 @Repository(value = "jpaRoleDao")
-public class JpaRoleDao extends  JPAAbstractDao<Role> implements RoleDao {
+public class JpaRoleDao extends JPAAbstractDao<Role> implements RoleDao {
     @Override
     public Role findByType(String type) {
         TypedQuery<Role> query = createTypedQuery("SELECT r FROM Role r WHERE r.type = :type)");
         query.setParameter("type", UserRoleType.valueOf(type));
 
-        return query.getSingleResult();
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 }
--- src/main/java/com/mmashyr/staffmanager/persistence/impl/JpaUserDao.java
+++ src/main/java/com/mmashyr/staffmanager/persistence/impl/JpaUserDao.java
@@ -3,7 +3,7 @@ package com.mmashyr.staffmanager.persistence.impl;
 import com.mmashyr.staffmanager.model.User;
 import com.mmashyr.staffmanager.persistence.UserDao;
 import org.springframework.stereotype.Repository;
-
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
 /**
@@ -12,17 +12,19 @@ import javax.persistence.TypedQuery;
 @Repository(value = "jpaUserDao")
 public class JpaUserDao extends JPAAbstractDao<User> implements UserDao {
 
-
     public JpaUserDao() {
-
     }
 
     @Override
     public User findByLogin(String login) {
         TypedQuery<User> query = createTypedQuery("SELECT u FROM User u WHERE u.login = :login");
         query.setParameter("login", login);
-
-        return query.getSingleResult();
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 }
+
 
