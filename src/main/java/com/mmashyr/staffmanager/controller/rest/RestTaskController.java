93abbd89509866d069f85e9654dd91a9b499eebd
Mashyr Mark <mashyr.mark@gmail.com>
2016-11-25 16:03:01 +0200

    ResponseEntity used in RestControllers, methods to get by id added

--- src/main/java/com/mmashyr/staffmanager/controller/rest/RestTaskController.java
+++ src/main/java/com/mmashyr/staffmanager/controller/rest/RestTaskController.java
@@ -1,10 +1,15 @@
 package com.mmashyr.staffmanager.controller.rest;
 
 import com.mmashyr.staffmanager.model.Task;
+import com.mmashyr.staffmanager.model.Worker;
 import com.mmashyr.staffmanager.services.TaskService;
 import com.mmashyr.staffmanager.services.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,12 +26,23 @@ public class RestTaskController {
     @Autowired
     private TaskService taskService;
 
-    @RequestMapping(value = "/tasks")
-    private List<Task> getAllTasks() {
+    @RequestMapping(value = "/tasks", method = RequestMethod.GET)
+    private ResponseEntity<List<Task>> getAllTasks() {
         List<Task> tasks = taskService.getAll();
-
-        return tasks;
+        if(tasks == null){
+            return new ResponseEntity<List<Task>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Task>>(tasks, HttpStatus.OK);
     }
+    @RequestMapping(value = "/tasks/{id}", method = RequestMethod.GET)
+    private ResponseEntity<Task> getTaskById(@PathVariable("id") int id){
+        Task task = taskService.getById(id);
+        if(task == null){
+            return new ResponseEntity<Task>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Task>(task, HttpStatus.OK);
+    }
+
 }
 
 
--- src/main/java/com/mmashyr/staffmanager/controller/rest/RestWorkerController.java
+++ src/main/java/com/mmashyr/staffmanager/controller/rest/RestWorkerController.java
@@ -1,10 +1,15 @@
 package com.mmashyr.staffmanager.controller.rest;
 
+import com.mmashyr.staffmanager.model.Task;
 import com.mmashyr.staffmanager.model.Worker;
 import com.mmashyr.staffmanager.services.TaskService;
 import com.mmashyr.staffmanager.services.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,10 +26,21 @@ public class RestWorkerController {
     @Autowired
     private TaskService taskService;
 
-    @RequestMapping(value = "/workers")
-    private List<Worker> getAllWorkers() {
+    @RequestMapping(value = "/workers", method = RequestMethod.GET)
+    private ResponseEntity<List<Worker>> getAllWorkers() {
         List<Worker> workers = workerService.getAll();
+        if(workers == null){
+            return new ResponseEntity<List<Worker>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<Worker>>(workers, HttpStatus.OK);
+    }
 
-        return workers;
+    @RequestMapping(value = "/workers/{id}", method = RequestMethod.GET)
+    private ResponseEntity<Worker> getWorkerById(@PathVariable("id") int id){
+        Worker worker = workerService.getById(id);
+        if(worker == null){
+            return new ResponseEntity<Worker>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Worker>(worker, HttpStatus.OK);
     }
 }
